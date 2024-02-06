964f3b2affdcb2c3acda6b68ce27e76e92d653c7
Akash <kumawat.aakash515@gmail.com>
2024-02-06 14:04:28 +0530

    added slider and announcementpage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Announcement from '../components/Announcements'
+import Slider from '../components/Slider'
 
 const Home = () => {
   return (
     <div>
+    <Announcement/>
       <Navbar/>
+      <Slider/>
     </div>
   )
 }
