b67e6616c1603380d156c36d22153a673ae7ca5b
Akash <kumawat.aakash515@gmail.com>
2024-02-06 15:01:14 +0530

    project Done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,17 @@ import Navbar from '../components/Navbar'
 import Announcement from '../components/Announcements'
 import Slider from '../components/Slider'
 import Categories from '../components/Categories'
-
 import Products from '../components/Products'
 import Newsletter from '../components/NewsLetter'
 import Footer from '../components/Footer'
+
 
 const Home = () => {
   return (
     <div>
     <Announcement/>
       <Navbar/>
+     
       <Slider/>
       <Categories/>
       <Products/>
